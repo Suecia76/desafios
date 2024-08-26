2a4abc47d53e92f0920d7364282218c1902a3be1
Suecia76 <carrascosanti76@gmail.com>
2024-08-26 11:27:44 -0300

    Subo los archivos actualizados ademas de archivos nuevos como env

--- desafio 2/import.mjs
+++ desafio 2/import.mjs
@@ -1,1 +1,11 @@
-const { primos, borrar } = require("./index.js");
+import { esPrimo } from "./index.cjs";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const numero = process.env.NUMERO; // Asegúrate de que este valor esté definido en el archivo .env
+const resultado = esPrimo(numero)
+  ? `El número ${numero} es primo.`
+  : `El número ${numero} no es primo.`;
+
+console.log(resultado);
